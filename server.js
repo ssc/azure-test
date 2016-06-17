402d55078898dd85d999bac6bca45eb7e55cfc9d
Steven Chow <schow@linkedin.com>
2016-06-17 13:12:58 -0700

    Fix undefined body

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
     head = '<head><title>Awesome stuff</title></head>';
-    text = '<h1>ssc azure homepage! hello napat and ben and jason hello</h1>Code for this here:<a href="https://github.com/ssc/azure-test">https://github.com/ssc/azure-test</a>\n';
-    text += '<div>$ PORT=8080 node server.js</div>';
+    body = '<h1>ssc azure homepage! hello napat and ben and jason hello</h1>Code for this here:<a href="https://github.com/ssc/azure-test">https://github.com/ssc/azure-test</a>\n';
+    body += '<div>$ PORT=8080 node server.js</div>';
     content = '<html>' + head + '<text>' + body + '</body></html>'; 
     res.end(content);
 }).listen(process.env.PORT);
