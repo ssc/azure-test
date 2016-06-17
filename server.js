62279eb8fe61c7e8be639390ce1c9fcb040e035f
Jason Ko <jko@slideshare.com>
2016-06-17 12:35:56 -0700

    better html fomatting

--- server.js
+++ server.js
@@ -2,5 +2,9 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>ssc azure homepage! hello napat and ben and jason</h1>Code for this here:<a href="https://github.com/ssc/azure-test">https://github.com/ssc/azure-test</a>');
+    head = '<head><title>Awesome stuff</title></head>';
+    text = '<h1>ssc azure homepage! hello napat and ben and jason</h1>Code for this here:<a href="https://github.com/ssc/azure-test">https://github.com/ssc/azure-test</a>\n';
+    text += '<div>$ PORT=8080 node server.js</div>';
+    content = '<html>' + head + '<body>' + text + '</body></html>'; 
+    res.end(content);
 }).listen(process.env.PORT);
