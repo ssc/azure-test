4a3d6c01cf45428b8ba7b8927767bb47ac4165b6
Steven Chow <schow@linkedin.com>
2016-06-17 13:08:31 -0700

    Add hello

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+var azure = require('azure-storage');
+
 var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
     head = '<head><title>Awesome stuff</title></head>';
-    text = '<h1>ssc azure homepage! hello napat and ben and jason</h1>Code for this here:<a href="https://github.com/ssc/azure-test">https://github.com/ssc/azure-test</a>\n';
+    text = '<h1>ssc azure homepage! hello napat and ben and jason hello</h1>Code for this here:<a href="https://github.com/ssc/azure-test">https://github.com/ssc/azure-test</a>\n';
     text += '<div>$ PORT=8080 node server.js</div>';
-    content = '<html>' + head + '<body>' + text + '</body></html>'; 
+    content = '<html>' + head + '<text>' + body + '</body></html>'; 
     res.end(content);
 }).listen(process.env.PORT);
