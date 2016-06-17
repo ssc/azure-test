840710899ad8bdd2855e460b976292df1720498a
Benjamin Silver <benjamin@slideshare.com>
2016-06-17 13:22:12 -0700

    Style improvements

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ http.createServer(function (req, res) {
     head = '<head><title>Awesome stuff</title></head>';
     body = '<h1> ssc azure homepage! hello napat and ben and jason hello</h1>Code for this here:<a href="https://github.com/ssc/azure-test">https://github.com/ssc/azure-test</a>\n';
     body += '<div>$ PORT=8080 node server.js</div>';
-    content = '<html>' + head + '<text>' + body + '</body></html>'; 
+    style = '<style>body{background-image: url("http://cdn.nyanit.com/nyan2.gif");background-position: center center;background-size: auto 100%;}</style>'
+    content = '<html>' + head + '<text>' + body + '</body>' + style + '</html>'; 
     res.end(content);
 }).listen(process.env.PORT);
