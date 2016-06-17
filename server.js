d0e57e79ec8248c35213b776a7e6617dbdac7862
Steven Chow <schow@linkedin.com>
2016-06-17 12:12:05 -0700

    Add link to the code

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>ssc azure homepage! hello napat and ben</h1>');
+    res.end('<h1>ssc azure homepage! hello napat and ben</h1>Code for this here:<a href="https://github.com/ssc/azure-test">https://github.com/ssc/azure-test</a>');
 }).listen(process.env.PORT);
